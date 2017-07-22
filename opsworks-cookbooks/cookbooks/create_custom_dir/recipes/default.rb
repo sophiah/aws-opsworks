b21983eac1e40ebf9c132da54cccd82ac71ff387
Sophia Hsieh <sophia.tw@gmail.com>
2017-07-22 21:21:38 +0800

    test

--- opsworks-cookbooks/cookbooks/create_custom_dir/recipes/default.rb
+++ opsworks-cookbooks/cookbooks/create_custom_dir/recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+directory '/nfs-root-default' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
+
+file '/nfs-root-default/test-custom-json' do
+  content "try to get string #{node['string_test']} and get list #{node['list_test']}"
+  mode '0755'
+  owner 'web_admin'
+  group 'web_admin'
+end
